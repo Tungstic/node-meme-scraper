91408c457d32168965352b6a2a1232c84cb55a10
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-04-26 18:03:20 +0200

    Add node-fetch library to download html as a string

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import fs from 'node:fs';
-import { convert } from 'html-to-text';
+import http from 'node:http';
+/*
 
-// const https = require('node:https');
-
-// URL of the image
 const url =
   'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300';
 
 const file = fs.createWriteStream('memes/01.jpg');
 file.write(url);
+*/
+import fetch from 'node-fetch';
+
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const body = await response.text();
+
+console.log(typeof body);
