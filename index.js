f89ee56a9406f24ad77afc040d6619e6e821b00d
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-04-26 21:27:34 +0200

    Test code with only the first image

--- index.js
+++ index.js
@@ -3,37 +3,25 @@ import fetch from 'node-fetch';
 import { parse } from 'node-html-parser';
 
 // import http from 'node:http';
-/*
 
-
-const url =
-  'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300';
-
-const file = fs.createWriteStream('memes/01.jpg');
-file.write(url);
-*/
-
+// Download HTML from meme website as a string (body is a string)
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
 );
 const body = await response.text();
 
-console.log(typeof body);
-
-console.log(body.indexOf('images'));
-// const withoutHead = body.slice(172);
-
-// const parsedBody = withoutHead.split(`<img>`); This gives an object of the whole html string
-/* this gives me every character in html string separately regardless of word formations
-for (const tag of withoutHead) {
-  console.log(tag);
-}
-*/
-
+// Convert string "body" into an object
 const root = parse(body);
+
+// Get only img tags from the whole of html
 const onlyImageTags = root.querySelector('img');
 // console.log(onlyImageTags);
 
 // console.log(Object.values(onlyImageTags)); // Gives me numbers of the inner objects??
 // console.log(Object.values(onlyImageTags));
-console.log(onlyImageTags.rawAttrs);
+const img1 = onlyImageTags.rawAttrs;
+
+const url = img1;
+
+const file = fs.createWriteStream('memes/01.jpg');
+file.write(url);
