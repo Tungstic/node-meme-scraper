4e18fa6613ed8f59487ad362fe75d16e8ff28409
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-04-26 21:19:46 +0200

    Get value of src as an attribute of img tag

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import fs from 'node:fs';
-import http from 'node:http';
+import fetch from 'node-fetch';
+import { parse } from 'node-html-parser';
+
+// import http from 'node:http';
 /*
+
 
 const url =
   'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300';
@@ -8,7 +12,6 @@ const url =
 const file = fs.createWriteStream('memes/01.jpg');
 file.write(url);
 */
-import fetch from 'node-fetch';
 
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
@@ -16,3 +19,21 @@ const response = await fetch(
 const body = await response.text();
 
 console.log(typeof body);
+
+console.log(body.indexOf('images'));
+// const withoutHead = body.slice(172);
+
+// const parsedBody = withoutHead.split(`<img>`); This gives an object of the whole html string
+/* this gives me every character in html string separately regardless of word formations
+for (const tag of withoutHead) {
+  console.log(tag);
+}
+*/
+
+const root = parse(body);
+const onlyImageTags = root.querySelector('img');
+// console.log(onlyImageTags);
+
+// console.log(Object.values(onlyImageTags)); // Gives me numbers of the inner objects??
+// console.log(Object.values(onlyImageTags));
+console.log(onlyImageTags.rawAttrs);
