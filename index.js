c4eceb8d50a110d9f86cb064c0d697bf313c4e56
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-04-27 16:01:18 +0200

    Create an array of URL strings

--- index.js
+++ index.js
@@ -1,33 +1,71 @@
 import fs from 'node:fs';
+import axios from 'axios';
 import fetch from 'node-fetch';
 import { parse } from 'node-html-parser';
 
 // import http from 'node:http';
 
 // Download HTML from meme website as a string (body is a string)
-const response = await fetch(
+const downloadHTML = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
 );
-const body = await response.text();
+const body = await downloadHTML.text();
 
 // Convert string "body" into an object
 const root = parse(body);
 
 // Get only img tags from the whole of html
-const onlyImageTags = root.querySelector('img');
+const onlyImageTags = root.querySelectorAll('img');
 // console.log(onlyImageTags);
+// console.log(Object.keys(onlyImageTags)); // Gives me numbers of the inner objects??
 
-// console.log(Object.values(onlyImageTags)); // Gives me numbers of the inner objects??
-// console.log(Object.values(onlyImageTags));
-const img1 = onlyImageTags.rawAttrs; // This is a string "src="https.."" that needs to be trimmed
+const img1 = onlyImageTags[0]['rawAttrs']; // This is a string "src="https.."" that needs to be trimmed
+const img2 = onlyImageTags[1]['rawAttrs'];
+const img3 = onlyImageTags[2]['rawAttrs'];
+const img4 = onlyImageTags[3]['rawAttrs'];
+const img5 = onlyImageTags[4]['rawAttrs'];
+const img6 = onlyImageTags[5]['rawAttrs'];
+const img7 = onlyImageTags[6]['rawAttrs'];
+const img8 = onlyImageTags[7]['rawAttrs'];
+const img9 = onlyImageTags[8]['rawAttrs'];
+const img10 = onlyImageTags[9]['rawAttrs'];
 
-// Trim that string
+const imgArray = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
 
-function bareUrl(linkString) {
-  linkString = linkString.trim();
-  return linkString.slice(5, -1);
+// Trim the strings inside the array
+
+function bareUrl(arrOfStrings) {
+  return arrOfStrings.map((str) => {
+    str = str.trim();
+    str = str.slice(5, -1);
+    return str;
+  });
 }
 
-console.log(bareUrl(img1));
-// const file = fs.createWriteStream('memes/01.jpg');
-// file.write(url);
+bareUrl(imgArray);
+
+// console.log(Object.values(onlyImageTags));
+
+/* THIS WORKS, DO NOT CHANGE, JUST NEED TO WORK FOR ARRAY FOR IMAGES INSTEAD OF ONLY ONE
+
+
+// Use Axios to download an image
+async function download(url, filepath) {
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream',
+  });
+  return new Promise((resolve, reject) => {
+    response.data
+      .pipe(fs.createWriteStream(filepath))
+      .on('error', reject)
+      .once('close', () => resolve(filepath));
+  });
+}
+
+const remoteUrl = image1;
+const localPath = 'memes/01.jpg';
+
+await download(remoteUrl, localPath);
+*/
